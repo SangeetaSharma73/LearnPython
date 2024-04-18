7804d2cc141f012dd26d1cae46b75d5d2b75d8fb
Sangeeta Sharma <sangeeta136428sharma@gmail.com>
2024-04-19 00:20:20 +0530

    done

---
+++ 9.Set/1.Intro.py
@@ -0,0 +1,16 @@
+#set 
+#Unique collection
+#unordered
+#unindexed
+#Mutable
+lst=[1,2,3,1,1]
+print(lst)
+
+#creating a set
+
+#empty set 
+s={}
+print(s,type(s),id(s))
+
+s=set()
+print(s,type(s),id(s))

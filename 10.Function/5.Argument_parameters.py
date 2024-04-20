e2dc9a1d507a2e6ed3c4c72c25ba63dfda5f9cc3
Sangeeta Sharma <sangeeta136428sharma@gmail.com>
2024-04-21 01:06:07 +0530

    done changes

--- 10.Function/5.Argument_parameters.py
+++ 10.Function/5.Argument_parameters.py
@@ -27,3 +27,37 @@ intro('prateek','swim')
 #     print('And my hobby is',hobby)
     
 #arbitary arguments
+'''when number of values you want to pass is not known
+   Like we pass multiple values in print function
+   The values are being stored in tuple.
+'''
+def test(*args):
+    print(args)
+    print(type(args))
+    for i in args:
+        print(i,end=' ')
+a=test(2,3,4,'riya',2.3)
+print(a)
+test(2,4,5)
+print(type(test))
+
+#keyword arguments
+# variable number of key word arguments 
+#it stores the data in dictionary format
+
+def intro(**kwargs):
+    print(kwargs,type(kwargs))
+    for key,values in kwargs.items():
+        print(key,values,sep=" : ")
+        
+intro(name='Ritesh',age=23,hobbies=['Swim','read'])
+
+def mix(a,b,c,age=34,*args,**kwarks):
+    print(a,b,c)
+    print(age,type(age))
+    print(args,type(args))
+    print(kwarks,type(kwarks))
+# mix(2) #TypeError: mix() missing 2 required positional arguments: 'b' and 'c'
+mix(2,4,5)
+mix(2,4,5,45,6,8,9,name='mona',hobby='dance')
+

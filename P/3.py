1335854aac03ab94d869120d03db5531a3ee4eae
Sangeeta <sangeeta136428sharma@gmail.com>
2025-02-08 22:39:29 +0530

    problem

---
+++ P/3.py
@@ -0,0 +1,12 @@
+# table name = products 
+# columns = id, productName , productCategory , totalQuantitySold
+
+# select productName from products where totalQuantitySold>100;
+
+# where display productName and product category where the totalQuanttitySOld>AvgquantitySold;
+
+# select avg(totalQuantitySOld) from products 
+
+# # Set @avgSoldQuantity= 
+
+# select productName,productCategory from products where totalQuantitySold>avgSoldQuantity;
